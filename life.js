33d3d3a13c23a41b77ddd7bf490cc8a140f73012
SAGAR KUMAR <132209562+sagar-jha216@users.noreply.github.com>
2024-05-13 21:51:40 +0530

    Add files via upload

---
+++ life.js
@@ -0,0 +1,9 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.navList')
+rightNav=document.querySelector('.rightNav')
+    burger.addEventListner('click',()=>{
+        navbar.classList.toggle('v-class')
+        navList.classList.toggle('v-class')
+        navbar.classList.toggle('h-class')
+    })
